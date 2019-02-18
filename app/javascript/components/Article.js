fbcb87f9c3e3c3dd0b1bb04328fba3452415c0f7
thanhthedev <huythanhdao@gmail.com>
2019-02-19 09:57:48 +1000

    added an Article component

--- app/javascript/components/Article.js
+++ app/javascript/components/Article.js
@@ -14,14 +14,25 @@ class Article extends React.Component {
           <div className="article-meta-details">
             <small>
               Created by: {this.props.author}, &nbsp;
-              <Timestamp time={this.props.created_at} precision={3} />,
+              <Timestamp time={this.props.created_at} precision={4} />,
               last_updated:{" "}
-              <Timestamp time={this.props.updated_at} precision={3} />
+              <Timestamp time={this.props.updated_at} precision={4} />
             </small>
           </div>
         </div>
       </React.Fragment>
     );
+  }
+  componentDidMount() {
+    let self = this;
+    setInterval(() => self.forceUpdate(), 5000);
+  }
+
+  componentWillUnmount() {
+    if (this._timer) {
+      clearInterval(this_timer);
+      this._timer = null;
+    }
   }
 }
 
